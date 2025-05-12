303bf5d1d64c61f3fcb2f4faddca26459a690672
hbtpoprock <gunnuu@hotmail.com>
2025-05-12 13:49:23 +0700

    add unit test for users controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { NotFoundException } from '@nestjs/common';
+
+const mockUser = {
+  id: 1,
+  name: 'John Doe',
+  email: 'test',
+  password: 'test',
+  isActive: true,
+  creator_id: 102,
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a user', async () => {
+    jest.spyOn(service, 'findOne').mockResolvedValueOnce(mockUser);
+
+    const result = await controller.findOne('1');
+    expect(result).toEqual(mockUser);
+    expect(service.findOne).toHaveBeenCalledWith(1);
+  });
+
+  it('should throw NotFoundException if user not found', async () => {
+    jest
+      .spyOn(service, 'findOne')
+      .mockRejectedValueOnce(new NotFoundException());
+
+    await expect(controller.findOne('999')).rejects.toThrow(NotFoundException);
   });
 });
